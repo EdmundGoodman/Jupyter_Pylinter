e52cc18fa894c4af383078facaf49fe26dd63ef8
EdmundGoodman <egoodman3141@gmail.com>
2021-10-29 19:45:56 +0100

    Started adding infrastructure to make tool into a package

---
+++ jupylint/_test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Tests for the library
+"""
+
+import unittest
+
+if __name__ == "__main__":
+    unittest.main()
+    print("All tests passed")
