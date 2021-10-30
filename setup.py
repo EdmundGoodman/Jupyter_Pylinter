d48636d642f96df4cb32bf6bf25ebd8bdb4ee42c
EdmundGoodman <egoodman3141@gmail.com>
2021-10-30 01:46:06 +0100

    Updated version numbers and dependencies for v1.0.0 packaging

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name = 'jupylint',
-    version = '0.1.0',
+    version = '1.0.0',
     license='MIT',
     description = 'A tool to run pylint on Jupyter notebooks',
     long_description=long_description,
@@ -14,9 +14,10 @@ setup(
     author = 'Edmund Goodman',
     author_email = 'egoodman3141@gmail.com',
     url = 'https://github.com/EdmundGoodman/Jupyter_Pylinter',
-    download_url = 'https://github.com/EdmundGoodman/Jupyter_Pylinter/archive/refs/tags/v0.1.0.tar.gz',
+    download_url = 'https://github.com/EdmundGoodman/Jupyter_Pylinter/archive/refs/tags/v1.0.0.tar.gz',
     packages=find_packages(),
     keywords = ['Jupyter', 'Pylint', 'linter'],
+    install_requires = ['pylint'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
